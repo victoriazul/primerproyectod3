a9a84bfdb4269ae39788d3d3d9b237c485263ab7
victoriazul <victoriahuelin@gmail.com>
2017-11-23 21:50:15 +0100

    Initial commit

---
+++ scatterplot.js
@@ -0,0 +1,37 @@
+console.log("Hola mundo")
+
+d3.json("datosPartidos.js", function(data){
+    
+    console.log("Hola")
+    
+    window.data = window
+    
+    //Escalas
+    escalatamanio = d3.scaleLinear ()
+    .domain([0,10])
+    .range([0,500])
+    
+    ancho = 500
+    alto = 500
+    
+    partidos = data.partidos
+   contenedor = d3.select("#contenedor")
+            .append("svg")
+            .attr("width", ancho)
+            .attr("height", alto)
+    
+    contenedor
+    .selectAll("circle")
+    .data(partidos)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) {return d.mediaAutoubicacion})
+    .attr("r", 10)
+    .attr("cy", 10)
+    
+    
+    
+    
+    
+    
+})
